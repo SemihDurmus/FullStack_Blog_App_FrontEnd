f72907945c7aa444a9a8adee8ac8b592998a185c
SemihDurmus <semihdurmus@yahoo.com>
2021-03-05 01:05:50 +0100

    comment edit style

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -86,7 +86,7 @@ function AccountPage() {
         }
       );
       setUserData(result.data);
-      console.log(result.data);
+      //console.log(result.data);
     } catch ({ response }) {
       if (response) {
         console.log("No data");
